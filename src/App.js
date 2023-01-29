2593f62d488c77da4c5e417545166b6550f9c9c7
Annie <overheads165@gmail.com>
2023-01-30 00:58:39 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ function App() {
   return (
     <div class="container">
       <Card>
-      <img src={cat} class="card-img-top" alt="котик"/>
+        <img src={cat} class="card-img-top" alt="котик" />
         <h3>Котик</h3>
-        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p>
+          Привет, меня зовут Котик. Я очень люблю сидеть на диване и смотреть
+          телевизор.</p>
       </Card>
       <Card>
         <h5 class="card-title">Card title</h5>
         <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Есть много вариантов Lorem Ipsum, но большинство из них
+          переустановлены с изменениями, например, добавлением прозвучавших
+          юмористических слов или слов, которые даже не похожи на латинские.
         </p>
         <a href="#" class="btn btn-primary">
           Go somewhere
